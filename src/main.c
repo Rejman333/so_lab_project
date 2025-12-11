eea7e59bce5bd1fe3af6dab893745643129f5308
rejman333 <janswidzinskiprograming@gmail.com>
2025-12-11 10:59:55 +0100

    Argv for configuration added, with parsing

--- src/main.c
+++ src/main.c
@@ -15,15 +15,56 @@
 int operator_pid = -1;
 int system_commander_pid = -1;
 
-void cleanup() {
-    print_msg("Cleanup started...");
+int starting_drones_count = 10;
+int resupply_interval = 1000000; // 1s in us
 
-    pid_t pid;
-    while ((pid = wait(NULL)) > 0) {
-        print_msg("Process ended: %d", pid);
+int maximum_charge_time = 10000000; //10s in us
+int max_loading_circles = 5;
+
+void process_argv(int argc, char* argv[]) {
+    if (argc > 1) {
+        starting_drones_count = atoi(argv[1]);
+        if (starting_drones_count <= 0) {
+            print_error("starting_drones_count must be > 0");
+            exit(1);
+        }
     }
 
-    print_msg("Cleanup complete.");
+    if (argc > 2) {
+        resupply_interval = atoi(argv[2]);
+        if (resupply_interval <= 0) {
+            print_error("resupply_interval must be > 0");
+            exit(1);
+        }
+    }
+
+    if (argc > 3) {
+        maximum_charge_time = atoi(argv[3]);
+        if (maximum_charge_time <= 0) {
+            print_error("maximum_charge_time must be > 0");
+            exit(1);
+        }
+    }
+
+    if (argc > 4) {
+        max_loading_circles = atoi(argv[4]);
+        if (max_loading_circles <= 0) {
+            print_error("max_loading_circles must be > 0");
+            exit(1);
+        }
+    }
+
+    if (argc > 5) {
+        print_error("Maximum argument count is 4");
+        exit(1);
+    }
+
+    print_msg("=== Starting Configuration ===");
+    print_msg("starting_drones_count = %d", starting_drones_count);
+    print_msg("resupply_interval = %d", resupply_interval);
+    print_msg("maximum_charge_time = %d", maximum_charge_time);
+    print_msg("max_loading_circles = %d", max_loading_circles);
+
 }
 
 int creat_operator() {
@@ -54,16 +95,30 @@ int creat_system_commander() {
     return pid;
 }
 
+void cleanup() {
+    print_msg("Cleanup started...");
+
+    pid_t pid;
+    while ((pid = wait(NULL)) > 0) {
+        print_msg("Process ended: %d", pid);
+    }
+
+    print_msg("Cleanup complete.");
+}
+
 int main(int argc, char *argv[]) {
     setup_print(PROCESS_NAME, PROCESS_COLOR);
+    process_argv(argc, argv);
     signal(SIGINT, SIG_IGN);
     //Optional
     //signal(SIGTERM, SIG_IGN);
+
     print_msg("Started");
 
     operator_pid = creat_operator();
     system_commander_pid = creat_system_commander();
 
     cleanup();
+
     return 0;
 }
